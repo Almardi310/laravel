927a87a33ed323a6038ea38f24a3e862ce47d844
Almardi310 <391116310@qu.edu.sa>
2023-07-26 12:57:48 +0300

    Add UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+
+        User::truncate();
+
+
+        User::create([
+            'name' => 'test',
+            'email' => 'test@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+
+
+        User::create([
+            'name' => 'test2',
+            'email' => 'test2@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
